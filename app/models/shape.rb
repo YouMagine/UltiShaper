27b30a9e4c9b3e42d0c71dbe000efe44f7f8dc52
Erik de Bruijn <github@erikdebruijn.nl>
2013-03-21 17:38:12 +0100

    Added client side validations.

--- app/models/shape.rb
+++ app/models/shape.rb
@@ -1,3 +1,21 @@
 class Shape < ActiveRecord::Base
   attr_accessible :name, :xmldata
+  
+  validates :name, :presence => true
+  validates :name, :length => { :minimum => 2 }
+  validates :name, :uniqueness => true
+  validates :name, :format => { 
+  	:with => /\A[a-zA-Z]/,
+    :message => "Only letters (A-Z) allowed as first character."
+  }
+
+  validates :name, :format => { 
+  	:with => /^[A-Za-z][A-Za-z0-9]*$/, 
+  	:message => "Only alphanumeric characters allowed (A-Z and 0-9)." 
+  }
+  validates :name, :exclusion => { 
+  	:in => %w(ultishaper ultimaker youmagine), 
+  	:message => "this is a reserved name."
+  }
+
 end
