005bd49ab8cabb51c6a308c90aa340795e98fe4f
Erik de Bruijn <github@erikdebruijn.nl>
2013-06-27 14:08:41 +0200

    b64toBlob utility function that's needed for uploading the screenshot right onto Youmagine.

--- public/assets/js/utils.js
+++ public/assets/js/utils.js
@@ -1,3 +1,26 @@
+function b64toBlob(b64Data, contentType, sliceSize) {
+    contentType = contentType || '';
+    sliceSize = sliceSize || 1024;
+
+    function charCodeFromCharacter(c) {
+        return c.charCodeAt(0);
+    }
+
+    var byteCharacters = atob(b64Data);
+    var byteArrays = [];
+
+    for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+        var slice = byteCharacters.slice(offset, offset + sliceSize);
+        var byteNumbers = Array.prototype.map.call(slice, charCodeFromCharacter);
+        var byteArray = new Uint8Array(byteNumbers);
+
+        byteArrays.push(byteArray);
+    }
+
+    var blob = new Blob(byteArrays, {type: contentType});
+    return blob;
+}
+
 // uuid
 function s4() {
   return Math.floor((1 + Math.random()) * 0x10000)
